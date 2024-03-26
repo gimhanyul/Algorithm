7fa089776773e8ea261f262d89fe18ee292bae41
hanyulrich <126761282+gimhanyul@users.noreply.github.com>
2024-03-26 15:38:49 +0900

    Update K번째수.py

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,4 +1,4 @@
-def solution(array, commands):
+def solution1(array, commands):
     answer = []
     for num in commands:
         first = num[0]-1
@@ -7,3 +7,6 @@ def solution(array, commands):
         new_array = sorted(array[first:last])
         answer.append(new_array[find])
     return answer
+
+def solution2(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
