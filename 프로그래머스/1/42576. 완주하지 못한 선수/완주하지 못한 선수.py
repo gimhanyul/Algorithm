6db44d41e95867ed9a2d509dc53010699a32c0d5
hanyulrich <126761282+gimhanyul@users.noreply.github.com>
2024-09-03 17:19:35 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 45.22 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,13 @@
+def solution(participant, completion):
+    hash_map = {}
+    for name in participant:
+        if name in hash_map:
+            hash_map[name] += 1
+        else:
+            hash_map[name] = 1
+    for name in completion:
+        if name in hash_map:
+            hash_map[name] -= 1  
+    for name in hash_map:
+        if hash_map[name] != 0:
+            return name
