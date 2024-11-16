1177af36d1449917efe7991cf2f2f22c6f1f871b
hanyulrich <126761282+gimhanyul@users.noreply.github.com>
2024-11-16 19:10:51 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.62 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,33 +1,32 @@
 from collections import deque
 
-def solution(maps):
-    n, m = len(maps), len(maps[0])
-    # 방문한 위치를 체크할 배열
-    visited = [[False] * m for _ in range(n)]
-    
-    # 상하좌우 이동
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
-    
-    # BFS를 위한 큐 초기화 및 시작 위치 설정
-    queue = deque([(0, 0, 1)]) # x, y, distance
-    visited[0][0] = True
-    
+def bfs(start_x, start_y, end_x, end_y, graph):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    n, m = len(graph), len(graph[0])
+
+    visited = set([(start_x, start_y)])
+    queue = deque([(start_x, start_y, 1)]) 
+
     while queue:
         x, y, dist = queue.popleft()
-        
-        # 목적지에 도착한 경우
-        if x == n-1 and y == m-1:
+
+        if x == end_x and y == end_y:
             return dist
-        
-        # 상하좌우 이동
+
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            
-            # 맵 범위 내에 있고, 벽이 아니며, 방문하지 않았다면
-            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1 and not visited[nx][ny]:
-                visited[nx][ny] = True
-                queue.append((nx, ny, dist+1))
-    
-    # 목적지에 도달할 수 없는 경우
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m: 
+                if (nx, ny) not in visited: 
+                    if graph[nx][ny] == 1:  
+                        visited.add((nx, ny))
+                        queue.append((nx, ny, dist + 1)) 
+
     return -1
+
+def solution(maps):
+    n, m = len(maps), len(maps[0])
+    return bfs(0, 0, n - 1, m - 1, maps)
+
